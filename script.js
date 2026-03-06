bc455b5fea10892ec4679621ea24423ea20f0fd5
Pedro11 <pedrohenriquecardosomateus2022@gmail.com>
2026-03-06 12:07:05 -0300

    Arquivos organizados

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Deu certo")
+const nota1 = document.getElementById("nota1")
+const nota2 = document.getElementById("nota2")
+const resultado = document.getElementById("resultado")
+
+const button = document.getElementById("button")
+
+function calcularMedia(){
+    const valorNota1 = Number (nota1.value)
+    const valorNota2 = Number (nota2.value)
+    const media = (valorNota1 + valorNota2)/2
+    resultado.innerText = `A Média é ${media.toFixed(2)}`
+}
+
+button.addEventListener("click", calcularMedia)
